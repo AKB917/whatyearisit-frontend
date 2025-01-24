34cbb8dbe10fe3e8c9f0addbe0b80cd0ce854e5c
Your name <Your email>
2025-01-24 16:03:32 +0100

    url front backend setup

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ const linkfetch = "https://whatyearisit-backend-indol-omega.vercel.app/users/yea
 fetch(linkfetch)
 .then(response => response.json())
 .then(data => {
-    document.querySelector('#year').textContent = data.Date
+    document.querySelector('#year').textContent = data.year
 })
